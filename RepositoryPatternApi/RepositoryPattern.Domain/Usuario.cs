43f00f24708804d88885c7a4fcc828090747c291
Arnaldo Vieira <limup@outlook.com>
2022-10-05 23:38:09 -0300

    Creating and implementing EF MySQL by code first

--- RepositoryPatternApi/RepositoryPattern.Domain/Usuario.cs
+++ RepositoryPatternApi/RepositoryPattern.Domain/Usuario.cs
@@ -1,11 +1,17 @@
 namespace RepositoryPattern.Domain
 {
-    public class Usuario
+    public class Usuario : Entity
     {
         public string Nome { get; set; }
         public int Email { get; set; }
         public int Idade { get; set; }
 
+        public Usuario(Guid id, string nome, int email, int idade) : base(id)
+        {
+            Nome = nome;
+            Email = email;
+            Idade = idade;
+        }
         
     }
     
