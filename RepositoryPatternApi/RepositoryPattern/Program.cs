7baae2082574b933bdb1e1526c4c87515023ddb2
Arnaldo Vieira <limup@outlook.com>
2022-10-06 23:31:53 -0300

    Set configuration MySQL, ConnectionString

--- RepositoryPatternApi/RepositoryPattern/Program.cs
+++ RepositoryPatternApi/RepositoryPattern/Program.cs
@@ -1,11 +1,26 @@
+using RepositoryPattern.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Data.Repositories.Abstractions;
+using RepositoryPattern.Data.Repositories;
+using RepositoryPattern.Core.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("Default"), 
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Default"))));
+
+builder.Services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+builder.Services.AddScoped<ICarroRepository, CarroRepository>();
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 
 var app = builder.Build();
 
